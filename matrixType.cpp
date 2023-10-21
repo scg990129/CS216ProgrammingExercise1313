f96a330a239a1dee78b9687137945212c2aebb76
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-10-21 13:35:52 -0700

    good input + output

--- matrixType.cpp
+++ matrixType.cpp
@@ -2,4 +2,52 @@
 // Created by Jacob Yeung on 10/18/23.
 //
 
+#include <sstream>
 #include "matrixType.h"
+#include <iostream>
+#include <vector>
+
+matrixType::~matrixType() {
+    for(int** ptrRow = matrix; ptrRow < matrix + this->getRowM(); ptrRow++){
+        delete[] *ptrRow;
+    }
+    delete[] this->matrix;
+}
+matrixType::matrixType(int rowM, int colN, std::istream &is): matrixType(rowM, colN){ // empty matrix
+    string rowString;
+    int element;
+        for (int row = 0; row < this->getRowM(); row++){
+        getline(is >> std::ws, rowString) ;
+        stringstream ss(rowString);
+        for(int col = 0; col < this->getColN(); col++){
+            ss >> element;
+            this->matrix[row][col] = element;
+        }
+    }
+}
+matrixType::matrixType(int rowM, int colN){ // empty matrix
+    this->rowM = rowM;
+    this->colN = colN;
+    matrix = new int *[this->getRowM()];
+    for (int i = 0; i < rowM; i++) {
+        matrix[i] = new int[this->colN];
+    }
+}
+
+int matrixType::getRowM() const{
+    return this->rowM;
+}
+int matrixType::getColN() const{
+    return this->colN;
+}
+
+std::ostream &operator<<(std::ostream &os, const matrixType &type) {
+    for(int** ptrRow = type.matrix; ptrRow < type.matrix + type.getRowM(); ptrRow++){
+        for(int* ptrCol = *ptrRow; ptrCol < *ptrRow + type.getColN(); ptrCol++){
+            os << *ptrCol << " ";
+        }
+        os << std::endl;
+    }
+    return os;
+}
+
--- matrixType.h
+++ matrixType.h
@@ -5,10 +5,32 @@
 #ifndef CS216PROGRAMMINGEXERCISE1313_MATRIXTYPE_H
 #define CS216PROGRAMMINGEXERCISE1313_MATRIXTYPE_H
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 class matrixType {
+protected:
+    int rowM, colN; // row = m, col = n
+    int **matrix; //= int[5][6]; // 5 X 6: 5 row x 6 col
+public:
+    matrixType(int rowM, int colN, std::istream &is); // empty matrix
+    matrixType(int rowM, int colN); // empty matrix
+    ~matrixType();
 
+    int getRowM() const;
+    int getColN() const;
+
+    friend std::ostream &operator<<(std::ostream &os, const matrixType &type);
+//    friend std::istream &operator>>(std::istream &is, vector<matrixType> type);
+
+    friend matrixType operator+(const matrixType &type1, const matrixType &type2);
+    friend matrixType operator-(const matrixType &type1, const matrixType &type2);
+    // [1A, 1B, 1C]
+    // [2A, 2B, 2C]
+    // [3A, 3B, 3C]
+    friend matrixType operator*(const matrixType &type1, const matrixType &type2);
 };
-
 
 #endif //CS216PROGRAMMINGEXERCISE1313_MATRIXTYPE_H
